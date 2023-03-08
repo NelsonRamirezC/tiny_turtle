b3e2c8771c9b08bca2413fc8c8e680b7fb519b4a
Nelson Ramirez <xhyskoax@gmail.com>
2023-03-08 20:09:10 -0300

    corrigiendo ruta de importación controlador usuarios

--- routes/usuarios.routes.js
+++ routes/usuarios.routes.js
@@ -1,8 +1,9 @@
 import express from 'express';
+import { getUsers } from '../controllers/users.controller.js'
 const router = express.Router();
 
-router.get("/api/productos", (req, res) => {
-    res.send("ruta productos")
+router.get("/api/productos", getUsers, (req, res) => {
+
 });
 
 router.get("/api/producto/:id", (req, res) => {
